c88a51b1db15066567d037798dad60dce4be989f
Jaison Joseph <jaisonj2001@gmail.com>
2022-11-07 02:06:57 -0500

    record audio temporarily

---
+++ js/documented/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+// socket io mounts on top of node's http server?
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    // console.log("got a request");
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected");
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
